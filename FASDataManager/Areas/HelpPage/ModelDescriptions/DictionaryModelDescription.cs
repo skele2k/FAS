629e4be69e1e6bbd7359fddbce3b0867a55c919e
Tsatsralt Light <tsatsralt2000@gmail.com>
2020-07-27 12:45:44 +0800

    Added the web api project

---
+++ FASDataManager/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace FASDataManager.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ FASDataManager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FASDataManager.WebApiApplication" Language="C#" %>
