9458bb808bc040c2d114eac3ee279f9bb09d6e09
Tsatsralt Light <tsatsralt2000@gmail.com>
2020-07-28 15:02:30 +0800

    wire up login form to API

---
+++ FASAdmin/Helpers/IAPIHelper.cs
@@ -0,0 +1,10 @@
+﻿using FASAdmin.Models;
+using System.Threading.Tasks;
+
+namespace FASAdmin.Helpers
+{
+    public interface IAPIHelper
+    {
+        Task<AuthenticatedUser> Authenticate(string username, string password);
+    }
+}
--- FASAdmin/ViewModels/LoginViewModel.cs
+++ FASAdmin/ViewModels/LoginViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using FASAdmin.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,12 @@ namespace FASAdmin.ViewModels
     {
         private string _userName;
         private string _password;
+        private IAPIHelper _apiHelper;
+        public LoginViewModel(IAPIHelper apiHelper)
+        {
+            _apiHelper = apiHelper;
+        }
+
         public string UserName
         {
             get { return _userName; }
@@ -48,9 +55,16 @@ namespace FASAdmin.ViewModels
                 return output;
             }
         }
-        public void LogIn()
+        public async Task LogIn()
         {
-            
+            try
+            {
+                var result = await _apiHelper.Authenticate(UserName, Password);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
