53c582ebfbec524365b37c2080a05b96ecebbc41
Tsatsralt Light <tsatsralt2000@gmail.com>
2020-08-03 18:02:31 +0800

    started Excel exporter

--- FASTAdmin/Controls/AdminControl.xaml.cs
+++ FASTAdmin/Controls/AdminControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FASTAdmin.excelHelper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -27,7 +28,7 @@ namespace FASTAdmin.Controls
 
         private void exportToExcelMenuItem_Click(object sender, RoutedEventArgs e)
         {
-
+            xlHelper.Exporter();
         }
 
         private void addStaffMenuItem_Click(object sender, RoutedEventArgs e)
---
+++ FASTAdmin/excelHelper/xlHelper.cs
@@ -0,0 +1,21 @@
+﻿using FASLib.DataAccess;
+using FASLib.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+
+namespace FASTAdmin.excelHelper
+{
+    public static class xlHelper
+    {
+        public static void Exporter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
